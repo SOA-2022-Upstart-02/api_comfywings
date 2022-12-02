840360292bc457fde00d357f160b791aae007d7e
Daniel Kong <danielkong24@gmail.com>
2022-12-02 19:59:37 +0800

    [WIP] Added Currency Representer

---
+++ app/application/services/retrieve_currencies.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'dry/transaction'
+require 'digest'
+
+module ComfyWings
+  module Service
+    # Retrieves array of all currencies
+    class RetrieveCurrencies
+      include Dry::Transaction
+
+      step :retrieve_all
+
+      private
+
+      NOT_FOUND_ERR = "Currencies not found."
+      DB_ERR = "We encountered an issue accessing the database."
+
+      def retrieve_all
+        currency_list = Repository::For.klass(Entity::Currency).all
+
+        if currency_list
+          Success(Response::ApiResult.new(status: :ok, message: currency_list))
+        else
+          Failure(Response::ApiResult.new(status: :not_found, message: NOT_FOUND_ERR))
+        end
+      rescue StandardError => e
+        Failure(Response::ApiResult.new(status: :internal_errpr, message: DB_ERR))
+      end
+
+    end
+  end
+end
---
+++ app/presentation/representers/currencies_representer.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'roar/decorator'
+require 'roar/json'
+
+require_relative 'openstruct_with_links'
+require_relative 'trip_representer'
+
+module ComfyWings
+  module Representer
+    # Represents list of currencies
+    class CurrenciesList < Roar::Decorator
+      include Roar::JSON
+
+      collection :trips, extend: Representer::Currency, lass: Representer::OpenStructWithLinks
+    end
+  end
+end
